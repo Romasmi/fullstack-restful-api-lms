d9a5868fb1eb7fcec8e3a1b12add4701ca054b72
romasmi <romasmi@ro.ru>
2022-05-28 13:25:00 +0300

    Add config

---
+++ backend/config/common.php
@@ -0,0 +1,6 @@
+<?php
+
+require 'database.php';
+
+const STUDENTS_PER_PAGE = 5;
+const COOKIE_LIVETIME = 3600 * 24 * 30;
---
+++ backend/config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+const DATABASE = [
+    'host' => 'romasmg7.beget.tech',
+    'port' => '3306',
+    'name' => 'romasmg7_lms',
+    'user' => 'romasmg7_lms',
+    'password' => 'H5NZFcR%'
+];
